0079275e380f3a742a193677c6292a31019b1cf0
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2019-09-28 00:57:30 +0300

    add sliding up panel

--- app/map/index.js
+++ app/map/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Dimensions } from 'react-native';
+import { View, Dimensions, Text, StyleSheet, Animated } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import SlidingUpPanel from 'rn-sliding-up-panel';
 export const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 
 class MapScreen extends React.Component {
@@ -8,10 +9,16 @@ class MapScreen extends React.Component {
     title: 'Map',
   };
 
+  static defaultProps = {
+    draggableRange: { top: SCREEN_HEIGHT * 0.75, bottom: 50 },
+  };
+
+  _draggedValue = new Animated.Value(50);
+
   render() {
     return (
       <View
-        style={{ flex: 1, height: SCREEN_HEIGHT }}
+        style={{ flex: 1, backgroundColor: 'white' }}
       >
         <MapView
           provider={PROVIDER_GOOGLE}
@@ -32,9 +39,48 @@ class MapScreen extends React.Component {
             coordinate={{ latitude: 28.469619, longitude: 77.038272 }}
           />
         </MapView>
+        <SlidingUpPanel
+          ref={c => (this._panel = c)}
+          draggableRange={this.props.draggableRange}
+          animatedValue={this._draggedValue}
+          snappingPoints={[360]}
+          height={SCREEN_HEIGHT * 0.75}
+          friction={0.5}
+        >
+          <View style={styles.panel}>
+            <View style={styles.panelHeader}>
+              <Text style={styles.textHeader}>Sliding Up Panel</Text>
+            </View>
+            <View styles={styles.container}>
+              <Text>Bottom sheet content</Text>
+            </View>
+          </View>
+        </SlidingUpPanel>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f8f9fa',
+  },
+  panel: {
+    flex: 1,
+    backgroundColor: 'white',
+    position: 'relative',
+  },
+  panelHeader: {
+    height: 50,
+    backgroundColor: '#b197fc',
+    justifyContent: 'flex-end',
+    padding: 24,
+  },
+  textHeader: {
+    fontSize: 14,
+    color: '#FFF',
+  },
+});
 
 export default MapScreen;
