7395c2f77d7db2988d7158c6ccda3d33f0c2462d
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2019-09-27 23:44:05 +0300

    add map

--- app/map/index.js
+++ app/map/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Dimensions } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+export const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 
 class MapScreen extends React.Component {
   static navigationOptions = {
@@ -8,8 +10,28 @@ class MapScreen extends React.Component {
 
   render() {
     return (
-      <View>
-        <Text>MapScreen</Text>
+      <View
+        style={{ flex: 1, height: SCREEN_HEIGHT }}
+      >
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={{ width: SCREEN_WIDTH, height: SCREEN_HEIGHT }}
+          initialRegion={{
+            latitude: 28.623999,
+            longitude: 77.383707,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05,
+          }}
+        >
+          <Marker
+            pinColor={'green'}
+            coordinate={{ latitude: 77.383707, longitude: 77.383707 }}
+          />
+          <Marker
+            pinColor={'blue'}
+            coordinate={{ latitude: 28.469619, longitude: 77.038272 }}
+          />
+        </MapView>
       </View>
     );
   }
